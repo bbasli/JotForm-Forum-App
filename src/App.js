9b987e65f1d7856f2f07038856909de660d44167
bbasli <bgrbasli@gmail.com>
2020-10-14 14:56:07 +0300

    Added NotFound page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Forum from "./containers/Forum/Forum";
-import Login from './containers/Login/Login';
-import NewQuestion from './containers/NewQuestion/NewQuestion';
+import Login from "./containers/Login/Login";
+import NewQuestion from "./containers/NewQuestion/NewQuestion";
 import Answer from "./containers/Answer/Answer";
-import { Route, Switch } from "react-router-dom";
+import NotFound from "./containers/NotFound/NotFound";
 
 class App extends Component {
   render() {
     return (
       <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/questions" component={Forum} />
-          <Route path="/questions/from=:offset" component={Forum} />
-          <Route path="/new-question" component={NewQuestion} />
-          <Route path="/answers/:id" component={Answer}/>
+        <Redirect from="/questions" to="/" />
+        <Route path="/" exact component={Forum} />
+        <Route path="/login" component={Login} />
+        <Route path="/questions/from=:offset" component={Forum} />
+        <Route path="/new-question" component={NewQuestion} />
+        <Route path="/answers/:id" component={Answer} />
+        <Route component={NotFound} />
       </Switch>
     );
   }
---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const notFound = (props) => {
+  return (
+    <div>
+      <h2>Page Not Found!</h2>
+      <a href="/">Click here</a>&nbsp;
+      <span>to Forum Page</span>
+    </div>
+  );
+};
+
+export default notFound;
